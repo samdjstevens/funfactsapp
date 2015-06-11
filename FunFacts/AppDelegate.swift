941345e59e3399f234f07c43cc5cbab2d8bf5709
Sam Stevens <sam@ticketline.co.uk>
2015-06-11 20:11:07 +0100

    set the app status bar style to be light

--- FunFacts/AppDelegate.swift
+++ FunFacts/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Set the status bar to be light app wide (depreciated in iOS 9)
+        application.setStatusBarStyle(UIStatusBarStyle.LightContent, animated: false)
+        
         return true
     }
 
